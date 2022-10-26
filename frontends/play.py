862757abb6e7c0d7ad382608e0cb4a4f3368914f
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2022-10-26 12:50:33 +0530

    implement console player

--- frontends/play.py
+++ frontends/play.py
@@ -1,10 +1,10 @@
 from tic_tac_toe.game.engine import TicTacToe
-from tic_tac_toe.game.players import RandomComputerPlayer
+from tic_tac_toe.game.players import RandomComputerPlayer, ConsolePlayer
 from tic_tac_toe.logic.models import Mark
 
 from console.renderers import ConsoleRenderer
 
-player1 = RandomComputerPlayer(Mark("X"))
+player1 = ConsolePlayer(Mark("X"))
 player2 = RandomComputerPlayer(Mark("O"))
 
 TicTacToe(player1, player2, ConsoleRenderer()).play()
